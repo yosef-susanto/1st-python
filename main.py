c2a95305164463ffe370ee690727501658fc9819
yosef <yosefsusanto7@gmail.com>
2023-03-31 18:34:30 +0700

    latihan pertama

--- main.py
+++ main.py
@@ -1,2 +1,12 @@
-print("hello world")
-print("apa kabar")
+"""
+Semua bahasa pemrograman sinteksis terdiri dari :
+1. Sekuensial : langkah beruntun
+2. Percabangan : langkah melompat jika kondisi terpenuhi
+3. Perulangan : mengulang langkah yang sama berkali-kali selama /sampai kondisi terpenuhi
+"""
+# Sekuensial
+print('ibu berkata, "pergi ke toko" ')
+print('budi menjawab, "baik, apa yang harus saya lakukan saat ke toko?" ')
+print('ibu berkata, "beli satu botol susu, dan jika ada telor beli 6" ')
+print('Maka budi berangkat ketoko')
+print('Dan mulai berbelanja')
